21f9a24e5395860e85ec14266c81c48567773fef
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-03-28 18:13:51 +0100

    feature wip : auth env setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  appname: 'gallery',
+  supabase: {
+    url: 'https://kuajpcqtgmmkbovmsthj.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1YWpwY3F0Z21ta2Jvdm1zdGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1NDUyNjMsImV4cCI6MjAyNzEyMTI2M30.1j3PBmFFnhWE_-X8My7_QlRWDiASOIc9xzgp6v6LXc0',
+  },
+};
